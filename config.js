7edbd901bf65d8b651ea283a37de2fa0f9b2a5eb
beuqk <96179328+beuqk@users.noreply.github.com>
2021-12-15 18:19:48 +0700

    z
    z

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Hey babe🖐',
+    introDesc: `I just have a thing to ask,be honest`,
+    btnIntro: 'Go',
+    title: 'I know you love me for sure 😙',
+    desc: 'Không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
+    btnYes: 'Yes , love so much 💖💖💖',
+    btnNo: '💔 Hell no 💔 ',
+    question:'Nói cho anh biết đi vì sao em thích anh 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì Thành Anh đẹp trai đó Ahiiiii 😊😊',
+    mess: 'Anh biết mà, Anh cũng thích em lắm 💖. Love you 3 thousand 💖',
+    messDesc: '💬💬 Text me babe 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/profile.php?id=100005254575586'
+}
